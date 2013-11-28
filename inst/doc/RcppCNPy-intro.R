90cfe1b06b1fcc87ed6fb02666de0b5c015ad5f5
Dirk Eddelbuettel <edd@debian.org>
2013-11-28 00:00:00 +0000

    version 0.2.1

---
+++ inst/doc/RcppCNPy-intro.R
@@ -0,0 +1,9 @@
+### R code from vignette source 'RcppCNPy-intro.Rnw'
+
+###################################################
+### code chunk number 1: RcppCNPy-intro.Rnw:53-55
+###################################################
+prettyVersion <- packageDescription("RcppCNPy")[["Version"]]
+prettyDate <- format(Sys.Date(), "%B %e, %Y")
+
+
